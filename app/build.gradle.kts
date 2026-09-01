a0bd7bcb0c8420c76e694e8348082acf215f075f
Diego Levano <diegoaltoperu2014@gmail.com>
2026-09-01 13:25:21 -0500

    Interfaz terminada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.8.0")
     implementation("com.google.android.material:material:1.14.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.2")
